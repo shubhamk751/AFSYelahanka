2892a5d4d5747efe48665ed4f2f5a503dfae74f3
Shubham Kumar Paswan <Shubham Kumar Paswan@ASI-5CD1462PK7>
2025-07-19 00:37:23 +0530

    CCA finalised

---
+++ AirForceSchoolYelahanka/AirForceSchoolYelahanka.Web/Controllers/ActivitiesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AirForceSchoolYelahanka.Web.Controllers
+{
+    public class ActivitiesController : Controller
+    {
+        [Route("cca-junior")]
+        public IActionResult CCAJunior()
+        {
+            return View();
+        }
+        [Route("cca-junior-plantation")]
+        public IActionResult CCAJuniorPlantation()
+        {
+            return View();
+        }
+        [Route("cca-junior-diwali")]
+        public IActionResult CCAJuniorDiwali()
+        {
+            return View();
+        }
+        [Route("cca-junior-othercca")]
+        public IActionResult CCAJuniorOtherCCA()
+        {
+            return View();
+        }
+    }
+}
