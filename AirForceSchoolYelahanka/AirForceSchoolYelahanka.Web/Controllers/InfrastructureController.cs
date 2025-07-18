3f864ee2c1858c5a6db3dc102c24ab82135e028d
Shubham Kumar Paswan <Shubham Kumar Paswan@ASI-5CD1462PK7>
2025-07-18 22:31:07 +0530

    prelim junior wing gallery

---
+++ AirForceSchoolYelahanka/AirForceSchoolYelahanka.Web/Controllers/InfrastructureController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AirForceSchoolYelahanka.Web.Controllers
+{
+    public class InfrastructureController : Controller
+    {
+        [Route("junior-wing")]
+        public IActionResult JuniorWing()
+        {
+            return View();
+        }
+    }
+}
