c5997245a744a1475ac0d416fe2762dd306e790c
Shubham Kumar Paswan <Shubham Kumar Paswan@ASI-5CD1462PK7>
2025-08-08 23:19:14 +0530

    public disclosure coded

---
+++ AirForceSchoolYelahanka/AirForceSchoolYelahanka.Web/Controllers/PublicDisclosureController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AirForceSchoolYelahanka.Web.Controllers
+{
+    public class PublicDisclosureController : Controller
+    {
+        [Route("mandatory-public-disclosure")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
