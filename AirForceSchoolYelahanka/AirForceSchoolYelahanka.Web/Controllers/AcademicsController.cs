8c8cf94d07d7976b2c67c2ba836f7a3ff85fbb2b
Shubham Kumar Paswan <Shubham Kumar Paswan@ASI-5CD1462PK7>
2025-08-11 21:26:38 +0530

    academics page coded

---
+++ AirForceSchoolYelahanka/AirForceSchoolYelahanka.Web/Controllers/AcademicsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AirForceSchoolYelahanka.Web.Controllers
+{
+    public class AcademicsController : Controller
+    {
+        [Route("/academics/junior")]
+        public IActionResult Junior()
+        {
+            return View();
+        }
+        [Route("/academics/senior")]
+        public IActionResult Senior()
+        {
+            return View();
+        }
+    }
+}
