7c7f55e02eb0af42ac15035d437991cbbdab1120
Shubham Kumar Paswan <Shubham Kumar Paswan@ASI-5CD1462PK7>
2025-08-18 19:37:07 +0530

    tcs issued fed done

--- AirForceSchoolYelahanka/AirForceSchoolYelahanka.Web/Controllers/AcademicsController.cs
+++ AirForceSchoolYelahanka/AirForceSchoolYelahanka.Web/Controllers/AcademicsController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AirForceSchoolYelahanka.Web.Services.Interfaces;
+using AirForceSchoolYelahanka.Web.ViewModel.TCUpload;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AirForceSchoolYelahanka.Web.Controllers
 {
     public class AcademicsController : Controller
     {
+        private readonly ITCService _tcService;
+        public AcademicsController(ITCService tcService)
+        {
+            _tcService = tcService;
+        }
         [Route("/academics/junior")]
         public IActionResult Junior()
         {
@@ -13,6 +21,25 @@ namespace AirForceSchoolYelahanka.Web.Controllers
         public IActionResult Senior()
         {
             return View();
+        }
+        [Route("/tcs-issued")]
+        public async Task<IActionResult> TCsIssued()
+        {
+            var tcs = await _tcService.GetTCsIssuedAsync();
+
+            var vmList = tcs.Select(tc => new TCUploadViewModel
+            {
+                Id = tc.Id,
+                StudentName = tc.StudentName,
+                Class = tc.Class,
+                Section = tc.Section,
+                AdmissionNo = tc.AdmissionNo,
+                IssuedOn = tc.IssuedOn,
+                Remarks = tc.Remarks,
+                FilePath = tc.FilePath
+            }).ToList();
+
+            return View(vmList);
         }
     }
 }
---
+++ AirForceSchoolYelahanka/AirForceSchoolYelahanka.Web/Services/TCService.cs
@@ -0,0 +1,35 @@
+﻿using AirForceSchoolYelahanka.Web.Data;
+using AirForceSchoolYelahanka.Web.Services.Interfaces;
+using AirForceSchoolYelahanka.Web.ViewModel.TCUpload;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirForceSchoolYelahanka.Web.Services
+{
+    public class TCService : ITCService
+    {
+        private readonly AppDbContext _context;
+        public TCService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<TCUploadViewModel>> GetTCsIssuedAsync()
+        {
+            var tcs = await _context.TCUpload.ToListAsync();
+
+            var vmList = tcs.Select(tc => new TCUploadViewModel
+            {
+                Id = tc.Id,
+                StudentName = tc.StudentName,
+                Class = tc.Class,
+                Section = tc.Section,
+                AdmissionNo = tc.AdmissionNo,
+                IssuedOn = tc.IssuedOn,
+                Remarks = tc.Remarks,
+                FilePath = tc.FilePath
+            }).ToList();
+
+            return vmList;
+
+        }
+    }
+}
