76153ef5275e80f3c16ced4da49b4f473e39a731
Shubham Kumar Paswan <Shubham Kumar Paswan@ASI-5CD1462PK7>
2025-09-15 20:30:04 +0530

    link made optional

--- AirForceSchoolYelahanka/AirForceSchoolYelahanka.Web/ViewModel/SectionItemViewModel.cs
+++ AirForceSchoolYelahanka/AirForceSchoolYelahanka.Web/ViewModel/SectionItemViewModel.cs
@@ -3,7 +3,7 @@
     public class SectionItemViewModel
     {
         public string Title { get; set; }
-        public string Link { get; set; }
-        public DateTime Date { get; set; }
+        public string? Link { get; set; }
+        public DateTime? Date { get; set; }
     }
 }
